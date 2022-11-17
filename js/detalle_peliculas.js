e1d1f59f9ec93b44ba90be3378e56d446824420b
franciscomanoukian <fmanoukian@udesa.edu.ar>
2022-11-17 01:18:35 -0300

    Avances detalle pelicula

--- js/detalle_peliculas.js
+++ js/detalle_peliculas.js
@@ -4,6 +4,7 @@ let qsObjLit = new URLSearchParams(qs);
 let idPeli = qsObjLit.get('id')
 
 let urlDetalle = `https://api.themoviedb.org/3/movie/${idPeli}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
+let urlWatchProviders = `https://api.themoviedb.org/3/movie/${idPeli}/watch/providers?api_key=81faef6942a31915ed87b416fbba64ba`
 
 fetch(urlDetalle).then(function (response) {
     return response.json()
@@ -20,7 +21,7 @@ fetch(urlDetalle).then(function (response) {
     let generos = document.querySelector('#generos_pelicula')
 
     // Preparo estructura
-    listaGeneros = ''
+    listaGeneros = 'AGREGARLE LINKS'
     generosTodos = data.genres
     for (let i = 0; i < generosTodos.length; i++) {
        listaGeneros+=`<a href="./detalle_genero.html" class="link_botones_generos" id="${generosTodos[i].id}">${generosTodos[i].name}</a> `
@@ -29,10 +30,34 @@ fetch(urlDetalle).then(function (response) {
     //Con toda la estructura html completa ahora la paso al DOM
     titulo.innerText = `${data.title}`;
     anioRating.innerText = `${data.release_date} - ⭐ ${data.vote_average}`
-    fotoPortada.innerHTML = ` <img src='https://image.tmdb.org/t/p/w500/${data.poster_path}'  alt="${data.title}" class="tapapelicula">` 
+    fotoPortada.innerHTML = ` <img src='https://image.tmdb.org/t/p/w500/${data.poster_path}'  alt="${data.title}" class="poster">` 
     duracion.innerText = `${data.runtime} minutos`
     sinopsis.innerText = `${data.overview}`
     generos.innerHTML = `${listaGeneros}`
+    
+}).catch(function (error) {
+    console.log(error);
+})
+
+fetch(urlWatchProviders).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+    let objLitProviders = data.results.AR.flatrate
+    console.log(objLitProviders);
+
+    //1 Capturo el elemento html en donde quiero hacer una modificación
+    let watchProviders = document.querySelector('#watchProviders')
+
+    let nombreProvider = ''
+    //Con toda la estructura html completa ahora la paso al DOM
+    
+    for (let i = 0; i < objLitProviders.length; i++) {
+        nombreProvider += objLitProviders[i].provider_name
+        
+    }
+
+    watchProviders.innerText = `AGREGAR LOGOS Y LINK Dónde ver: ${nombreProvider}`
     
 }).catch(function (error) {
     console.log(error);
