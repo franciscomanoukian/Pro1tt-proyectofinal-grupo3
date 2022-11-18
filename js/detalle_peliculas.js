99a6a10adfc7333e5adbc3203c2c638ef936e15c
franciscomanoukian <fmanoukian@udesa.edu.ar>
2022-11-18 01:14:39 -0300

    Avances detalle peliculas y correccion de errores

--- js/detalle_peliculas.js
+++ js/detalle_peliculas.js
@@ -5,7 +5,9 @@ let idPeli = qsObjLit.get('id')
 
 let urlDetalle = `https://api.themoviedb.org/3/movie/${idPeli}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
 let urlWatchProviders = `https://api.themoviedb.org/3/movie/${idPeli}/watch/providers?api_key=81faef6942a31915ed87b416fbba64ba`
+let urlGetRecommendations = `https://api.themoviedb.org/3/movie/${idPeli}/recommendations?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
 
+// Traigo datos de la película y los aplico al DOM
 fetch(urlDetalle).then(function (response) {
     return response.json()
 }).then(function (data) {
@@ -21,10 +23,10 @@ fetch(urlDetalle).then(function (response) {
     let generos = document.querySelector('#generos_pelicula')
 
     // Preparo estructura
-    listaGeneros = 'AGREGARLE LINKS'
+    listaGeneros = 'AGREGARLE QUERYSTRINGS'
     generosTodos = data.genres
     for (let i = 0; i < generosTodos.length; i++) {
-       listaGeneros+=`<a href="./detalle_genero.html" class="link_botones_generos" id="${generosTodos[i].id}">${generosTodos[i].name}</a> `
+       listaGeneros+=`<a href="./detalle_genero.html?genre_id="${generosTodos[i].id}" class="link_botones_generos" id="${generosTodos[i].id}">${generosTodos[i].name}</a> `
     }
 
     //Con toda la estructura html completa ahora la paso al DOM
@@ -39,11 +41,12 @@ fetch(urlDetalle).then(function (response) {
     console.log(error);
 })
 
+// Traigo datos de dónde ver la película y los aplico al DOM
 fetch(urlWatchProviders).then(function (response) {
     return response.json()
 }).then(function (data) {
     //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
-    let objLitProviders = data.results.AR.flatrate
+    let objLitProviders = data.results
     console.log(objLitProviders);
 
     //1 Capturo el elemento html en donde quiero hacer una modificación
@@ -57,8 +60,63 @@ fetch(urlWatchProviders).then(function (response) {
         
     }
 
-    watchProviders.innerText = `AGREGAR LOGOS Y LINK Dónde ver: ${nombreProvider}`
+    watchProviders.innerHTML = `AGREGAR LOGOS Y LINK Dónde ver: ${nombreProvider}`
     
 }).catch(function (error) {
     console.log(error);
 })
+
+// Traigo datos de películas similares recomendadas y las aplico al DOM
+fetch(urlGetRecommendations).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+    let arrayRecomendadas = data.results
+    console.log(data.results);
+
+    //1 Capturo el elemento html en donde quiero hacer una modificación
+    // let watchProviders = document.querySelector('#watchProviders')
+    let verRecomendaciones = document.querySelector('#verRecomendaciones');
+    let recomendadas = ''
+
+    //2 Recorro la información de la api y la organizo para mostarla en el html
+    for(let i=0; i<5; i++){
+        //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
+        recomendadas += `<article class="peliOSerie">
+                                <p class="nombrePeliOSerie">${arrayRecomendadas[i].title}</p>
+                                <img src='https://image.tmdb.org/t/p/w500/${arrayRecomendadas[i].poster_path}'  alt="Titanic" class="tapapelicula">
+                                <a href="./detalle_peliculas.html?id=${arrayRecomendadas[i].id}" class="linkadetalle">Ver más</a>
+                            </article>`
+    }
+    //Con toda la estructura html completa ahora la paso al DOM
+    verRecomendaciones.innerHTML = recomendadas;
+    
+}).catch(function (error) {
+    console.log(error);
+})
+
+let h3 = document.querySelector('h3')
+let botonRecomendaciones = document.querySelector('#botonVerRecomendaciones')
+let recomendacionesHidden = true
+
+if (recomendacionesHidden) {
+    botonRecomendaciones.innerText = 'Ver Recomendadas'
+    h3.style.display = 'none'
+    verRecomendaciones.style.display = 'none'
+}
+
+botonRecomendaciones.addEventListener('click', function (e) {
+    if (recomendacionesHidden) {
+        botonRecomendaciones.innerText = 'Ver Recomendadas'
+        h3.style.display = 'none'
+        verRecomendaciones.style.display = 'none'
+        recomendacionesHidden = false
+    } else {
+        botonRecomendaciones.innerText = 'Ocultar Recomendadas'
+        h3.style.display = 'flex'
+        verRecomendaciones.style.display = 'flex'
+        recomendacionesHidden = true
+    }
+})
+
+// Esconder seccion ver recomendaciones
