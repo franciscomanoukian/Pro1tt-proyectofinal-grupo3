2523c0b0ab9da744272655c6821e5bca8fef47cf
MateoLopezTeran <mlopezteran@udesa.edu.ar>
2022-11-19 21:12:07 -0300

    avances en favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -4,8 +4,11 @@ console.log(recupeStorage);
 
 let favoritos = JSON.parse(recupeStorage);
 
-let section = document.querySelector("#pelisFavoritas");
-let personajesFavoritos = ''; /* ---------------------->   OJALDRE ACA */
+let pelisFavoritas = document.querySelector("#pelisFavoritas");
+let arrayPeliculasFavoritas = '';
+
+let seriesFavoritas = document.querySelector('#seriesFavoritas');
+let arraySeriesFavoritas = '';
 
 console.log(favoritos);
 
@@ -15,25 +18,43 @@ if (favoritos == null || favoritos.length == 0) {
     /* No hay favoritos */
     section.innerHTML = '<p>No hay películas en favoritos</p>'
 } else {
-    for (let i = 0; i < favoritos.length; i++) {
-        let api = `https://api.themoviedb.org/3/movie/${favoritos[i]}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
-
-        fetch(api)
+    for (let i = 0; i < 5; i++) {
+        let apiPeliculas = `https://api.themoviedb.org/3/movie/${favoritos[i]}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
+        fetch(apiPeliculas)
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
-                let arrayPelisFavoritas = data;
-                console.log(arrayPelisFavoritas);
-                peliculasFavoritas += `<article class="peliOSerieReducido">
-                                            <p class="nombrePeliOSerie">${arrayPelisFavoritas.title}</p>
-                                            <img src="${arrayPelisFavoritas.poster_path}" alt="img" class="tapapelicula">
-                                            <a href="./detalle_peliculas.html?id=${arrayPelisFavoritas.id}" class="linkadetalle">Ver más</a>
+                console.log(data);
+                arrayPeliculasFavoritas += `<article class="peliOSerie">
+                                            <p class="nombrePeliOSerie">${data.title}</p>
+                                            <img src='https://image.tmdb.org/t/p/w500/${data.poster_path}' alt="img" class="tapapelicula">
+                                            <a href="./detalle_peliculas.html?id=${data.id}" class="linkadetalle">Ver más</a>
                                         </article>`
 
-            seccionPeliSerieReducida.innerHTML = peliculasFavoritas;
+            pelisFavoritas.innerHTML = arrayPeliculasFavoritas;
 
             }).catch(function (error) {
                 return error;
             });
         }
     }
+
+    /* for (let i = 0; i < 5; i++) {
+        let apiSeries = `https://api.themoviedb.org/3/tv/${favoritos[i]}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
+        fetch(apiSeries)
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+                arraySeriesFavoritas += `<article class="peliOSerie">
+                                            <p class="nombrePeliOSerie">${data.title}</p>
+                                            <img src='https://image.tmdb.org/t/p/w500/${data.poster_path}'  alt="" class="tapapelicula">
+                                            <a href="./detalle_peliculas.html?id=${data.id}" class="linkadetalle">Ver más</a>
+                                        </article>`
+
+            seriesFavoritas.innerHTML = arraySeriesFavoritas;
+
+            }).catch(function (error) {
+                return error;
+            });
+        } */
