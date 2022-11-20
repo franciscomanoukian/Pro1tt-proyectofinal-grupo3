2363b9aeaa58f27a3abc87600b61578eb90cd43d
franciscomanoukian <fmanoukian@udesa.edu.ar>
2022-11-20 13:42:25 -0300

    Termino detalle series. Falta que funcione boton agregar a favoritos

--- js/detalle_series.js
+++ js/detalle_series.js
@@ -3,3 +3,125 @@ let qsObjLit = new URLSearchParams(qs);
 
 let idSerie = qsObjLit.get('id')
 console.log(idSerie);
+
+let urlDetalle = `https://api.themoviedb.org/3/tv/${idSerie}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
+let urlWatchProviders = `https://api.themoviedb.org/3/tv/${idSerie}/watch/providers?api_key=81faef6942a31915ed87b416fbba64ba`
+let urlGetRecommendations = `https://api.themoviedb.org/3/tv/${idSerie}/recommendations?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
+
+
+// Traigo datos de la película y los aplico al DOM
+fetch(urlDetalle).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+    console.log(data);
+
+    //1 Capturo el elemento html en donde quiero hacer una modificación
+    let titulo = document.querySelector('.titulo');
+    let anioRating = document.querySelector('#año_y_rating');
+    let fotoPortada = document.querySelector('#foto_portada');
+    let duracion = document.querySelector('#duracion')
+    let sinopsis = document.querySelector('#sinopsis')
+    let generos = document.querySelector('#generos_pelicula')
+    let botonFavoritos = document.querySelector('#botonFavoritos')
+
+    // Preparo estructura
+    listaGeneros = ''
+    generosTodos = data.genres
+    for (let i = 0; i < generosTodos.length; i++) {
+       listaGeneros+=`<a href="./detalle_genero.html?id_genero=${generosTodos[i].id}" class="link_botones_generos" id="${generosTodos[i].id}">${generosTodos[i].name}</a> `
+    }
+
+    //Con toda la estructura html completa ahora la paso al DOM
+    titulo.innerText = `${data.name}`;
+    anioRating.innerText = `${data.first_air_date} - ⭐ ${data.vote_average}`
+    fotoPortada.innerHTML = ` <img src='https://image.tmdb.org/t/p/w500/${data.poster_path}'  alt="${data.title}" class="poster">` 
+    duracion.innerText = `${data.number_of_seasons} temporadas`
+    sinopsis.innerText = `${data.overview}`
+    generos.innerHTML = `${listaGeneros}`
+    
+}).catch(function (error) {
+    console.log(error);
+})
+
+// Traigo datos de dónde ver la película y los aplico al DOM
+fetch(urlWatchProviders).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+    let objLitProviders = data.results
+    console.log(objLitProviders);
+
+    //1 Capturo el elemento html en donde quiero hacer una modificación
+    let watchProviders = document.querySelector('#watchProviders')
+
+ 
+    //Con toda la estructura html completa ahora la paso al DOM
+    
+    if (objLitProviders.MX != undefined) {
+        let prove = objLitProviders.MX.buy[0];
+        watchProviders.innerHTML = `<p class="prove">Dónde ver: ${prove.provider_name}</p>
+                                    <img class="proveImg" src="https://image.tmdb.org/t/p/w500/${prove.logo_path}">`
+    }
+        
+    
+
+    watchProviders.innerHTML = `AGREGAR LOGOS Y LINK Dónde ver: ${nombreProvider}`
+    
+}).catch(function (error) {
+    console.log(error);
+})
+
+// Traigo datos de películas similares recomendadas y las aplico al DOM
+fetch(urlGetRecommendations).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+    let arrayRecomendadas = data.results
+    console.log(data.results);
+
+    //1 Capturo el elemento html en donde quiero hacer una modificación
+    // let watchProviders = document.querySelector('#watchProviders')
+    let verRecomendaciones = document.querySelector('#verRecomendaciones');
+    let recomendadas = ''
+
+    //2 Recorro la información de la api y la organizo para mostarla en el html
+    for(let i=0; i<5; i++){
+        //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
+        recomendadas += `<article class="peliOSerie">
+                                <p class="nombrePeliOSerie">${arrayRecomendadas[i].name}</p>
+                                <img src='https://image.tmdb.org/t/p/w500/${arrayRecomendadas[i].poster_path}'  alt="img" class="tapapelicula">
+                                <a href="./detalle_series.html?id=${arrayRecomendadas[i].id}" class="linkadetalle">Ver más</a>
+                            </article>`
+    }
+    //Con toda la estructura html completa ahora la paso al DOM
+    verRecomendaciones.innerHTML = recomendadas;
+    
+}).catch(function (error) {
+    console.log(error);
+})
+
+let h3 = document.querySelector('h3')
+let botonRecomendaciones = document.querySelector('#botonVerRecomendaciones')
+let recomendacionesHidden = true
+
+if (recomendacionesHidden) {
+    botonRecomendaciones.innerText = 'Ver Recomendadas'
+    h3.style.display = 'none'
+    verRecomendaciones.style.display = 'none'
+}
+
+botonRecomendaciones.addEventListener('click', function (e) {
+    if (recomendacionesHidden) {
+        botonRecomendaciones.innerText = 'Ver Recomendadas'
+        h3.style.display = 'none'
+        verRecomendaciones.style.display = 'none'
+        recomendacionesHidden = false
+    } else {
+        botonRecomendaciones.innerText = 'Ocultar Recomendadas'
+        h3.style.display = 'flex'
+        verRecomendaciones.style.display = 'flex'
+        recomendacionesHidden = true
+    }
+})
+
